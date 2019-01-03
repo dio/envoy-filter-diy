2febe12f830ba51e0ae4378f77543ab4f7dd110a
Dhi Aurrahman <dio@rockybars.com>
2019-01-03 22:25:52 +0700

    Add missing well-known names header
    Signed-off-by: Dhi Aurrahman <dio@rockybars.com>

---
+++ source/extensions/filters/http/well_known_names.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "common/config/well_known_names.h"
+
+namespace Envoy {
+namespace Extensions {
+namespace HttpFilters {
+
+/**
+ * Extended well-known http filter names.
+ */
+class ExtendedHttpFilterNameValues {
+public:
+  // Basic Auth filter
+  const std::string BasicAuth = "diy.basic_auth";
+
+};
+
+typedef ConstSingleton<ExtendedHttpFilterNameValues> ExtendedHttpFilterNames;
+
+} // namespace HttpFilters
+} // namespace Extensions
+} // namespace Envoy
